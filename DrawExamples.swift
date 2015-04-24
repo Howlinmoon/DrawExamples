624903a2eb208926a7110a1972718b2932372757
Jim Veneskey <github@strifecrafter.com>
2015-04-24 12:22:36 -0400

    More drawing - and displaying pictures too

--- DrawExamples.swift
+++ DrawExamples.swift
@@ -18,6 +18,10 @@ class DrawExamples: UIView {
         let screenSize: CGRect = UIScreen.mainScreen().bounds
         let screenWidth = screenSize.width
         let screenHeight = screenSize.height
+        //print ("Screen Width: \(screenWidth), Screen Height: \(screenHeight)"
+            
+        print ("Width: \(screenWidth)")
+        print ("Height: \(screenHeight)")
         
         
         // need a context for drawing - our canvas so to speak..
@@ -42,13 +46,16 @@ class DrawExamples: UIView {
         CGContextAddLineToPoint(context, 50, 50)
         */
         
+        /* basic rectangle
         var rectangle = CGRectMake(50, 50, 200, 400)
         CGContextAddRect(context, rectangle)
         
         
         // Actually draw the line
         CGContextStrokePath(context)
-
+        */
+        
+        /* Bad attempt at drawing concentric boxes
         for var xCoord = 10; xCoord <= Int(screenWidth) / 2; xCoord += 10 {
             rectangle = CGRectMake(CGFloat(xCoord), CGFloat(xCoord), screenWidth - CGFloat(xCoord), screenHeight - CGFloat(xCoord))
             CGContextAddRect(context, rectangle)
@@ -58,9 +65,38 @@ class DrawExamples: UIView {
             CGContextStrokePath(context)
             
         }
-
+        */
         
 
+        /*
+        CGContextMoveToPoint(context, 250, 100)
+        CGContextAddLineToPoint(context, 50, 250)
+        CGContextAddLineToPoint(context, 250, 350)
+        CGContextAddLineToPoint(context, 350, 250)
+        CGContextAddLineToPoint(context, 250,100)
+        
+        // Actually draw the lines
+        // CGContextStrokePath(context)
+        
+        CGContextSetFillColorWithColor(context, UIColor.redColor().CGColor)
+        CGContextFillPath(context)
+        
+        */
+        
+        
+        let birthdayCake = UIImage(named: "birthdayPicture.jpg")
+        let rocket = UIImage(named: "Rocket.jpg")
+        let legoMe = UIImage(named: "lego_me_small.png")
+        
+        /* draw normally
+        let location = CGPointMake(25,25)
+        rocket?.drawAtPoint(location)
+        */
+        
+        // Add a background
+        let entireScreen = UIScreen.mainScreen().bounds
+        rocket?.drawInRect(entireScreen)
+        
 
         
     }
